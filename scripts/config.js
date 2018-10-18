ac7bd3715cae832493feb4994734a2c4961edf20
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-18 11:41:38 -0400

    nothing

--- scripts/config.js
+++
@@ -1,3 +0,0 @@
-const OWKey = `88b430eb57686cffb62a9a7565a182f5`;
-const GoKey = `AIzaSyDX7NHyx9RpNMLF1Hbd6vwuXy_ESzR9oSE`;
-const USKey = `1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`;
