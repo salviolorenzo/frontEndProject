cc660a8318b1de72f947deccfd99a405e5932620
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-18 13:11:14 -0400

    adds images and basic js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
+const landingImage = document.querySelector('[data-landing]');
+const gallery = document.querySelector('[data-gallery]');
+const keyWord = `landscape`;
+const imageArray = [];
 // change landing photo on click
 
 // logic for interactive photo gallery
+function getImages(){
+    fetch(`https://api.unsplash.com/photos/random?client_id=${USKey}`)
+    .then(r => r.json())
+    .then(keepImage);
+}
+
+function drawImages(obj){
+    let img = document.createElement('img');
+    img.setAttribute('src', obj.urls.raw);
+    gallery.appendChild(img);
+}
+
+function keepImage(obj){
+    if(obj.location !== undefined){
+        drawImages(obj);
+    }
+}
     // Google vision for captions 
     // Display metadata onclick
 
 // logic for map plotting based on image location information
 
 // logic for passing location information from maps/image to weather 
+getImages();
