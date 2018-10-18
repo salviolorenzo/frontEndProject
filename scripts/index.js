6f88956dcbfe380bc2b7dec020e42ce0e2de1b3b
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-18 11:39:58 -0400

    adds basic skeleton files

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// change landing photo on click
+
+// logic for interactive photo gallery
+    // Google vision for captions 
+    // Display metadata onclick
+
+// logic for map plotting based on image location information
+
+// logic for passing location information from maps/image to weather 
