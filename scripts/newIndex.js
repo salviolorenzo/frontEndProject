a369666727a863fa5445a59414291b45ed7d17d0
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-20 18:39:56 -0400

    adjust js to fetch images from flickr api

--- scripts/newIndex.js
+++ scripts/newIndex.js
@@ -8,105 +8,94 @@
 const mapConatiner = document.querySelector("[data-map]");
 
 // get collection of photos using flickr.photos.search
-const searchUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickKey}&text=${userSearch}&format=json&nojsoncallback=1`;
+// const searchUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickKey}&text=${userSearch}&format=json&nojsoncallback=1`;
 // push IDs to an array
-IDs = obj.photos[index].id;
+// IDs = obj.photos[index].id;
+// Photo source URL = https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 //  geolocation
-const geoUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=${flickKey}&photo_id=${photoID}&format=json&nojsoncallback=1&auth_token=72157702427492494-a9c8f90feb96e087&api_sig=bd0421e2863ae103eb5608fabb9a70dd`;
+// const geoUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=${flickKey}&photo_id=${photoID}&format=json&nojsoncallback=1&auth_token=72157702427492494-a9c8f90feb96e087&api_sig=bd0421e2863ae103eb5608fabb9a70dd`;
 // get MetaData
-const exifUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=${flickKey}&photo_id=${photoID}&format=json&nojsoncallback=1`;
+// const exifUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=${flickKey}&photo_id=${photoID}&format=json&nojsoncallback=1`;
+
+// function handleSubmit(event) {
+// 	event.preventDefault();
+// 	console.log("Submit successful.");
+// 	let inputs = event.target.elements;
+// 	let userSearch = inputs.search.value;
+// 	// gallery.classList.remove("gallery-hidden");
+// 	// mapSection.classList.remove("map-weather-hidden");
+// 	getPhotos(userSearch);
+// }
+;
+
+function getPhotos(userSearch) {
+	fetch(
+		`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickKey}&text=${userSearch}&accuracy=16&sort=interestingness_desc&format=json&nojsoncallback=1`
+	)
+		.then(r => r.json())
+		.then(j => j.photos)
+		.then(getPhotoStats)
+		.then(drawImages);
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+	console.log("Submit successful.");
+	let inputs = event.target.elements;
+	let userSearch = inputs.search.value;
+	// gallery.classList.remove("gallery-hidden");
+	// mapSection.classList.remove("map-weather-hidden");
+	getPhotos(userSearch);
+}
+const searchForm = document.querySelector("[data-form]");
+
+searchForm.addEventListener("submit", handleSubmit);
+
+
+// takes original object, return array of objects photo id and image url
+function getPhotoStats(obj) {
+	let imagesArray = obj.photo;
+	let statArray = [];
+	for (image of imagesArray) {
+		let imageObj = {
+			'id': image.id,
+			'src': `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
+		};
+		statArray.push(imageObj);
+	}
+	return statArray;
+}
+
+const img = document.querySelector("[data-image]");
+const modal = document.querySelector("[data-modal]");
+const gallery = document.querySelector("[data-gallery]");
+
+function drawImages(arr) {
+	for (obj of arr) {
+		let image = document.createElement('img');
+		let index = arr.indexOf(obj);
+		image.setAttribute('src', arr[index].src);
+		gallery.appendChild(image);
+	}
+}
+
+// function getLocation(arr) {
+// 	for (object of arr) {
+// 		fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=${flickKey}&photo_id=${object.id}&format=json&nojsoncallback=1`)
+// 			// .then(r => r.json())
+// 			.then(k => k.location)
+// 			.then(x => {
+// 				object.location = { obj.long }
+// 			});
+// 		debugger;
+
+// 	}
+// }
+
+
 
 // creating first map instance
-map = new google.maps.Map(mapContainer, {
-	center: { lat: -34.397, lng: 150.644 },
-	zoom: 8
-});
-
-// logic for weather component
-// not refactored or tailored for receiving input
-
-const userCity = "atlanta";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${OWKey}`;
-
-// get and display temperature
-function getWeather() {
-	fetch(url)
-		.then(r => r.json())
-		.then(drawName)
-		.then(drawTemp)
-		.then(drawHum)
-		.then(drawPress)
-		.then(weather)
-		.catch(loadCache);
-}
-const body = document.querySelector("body");
-const display = document.querySelector("[data-display]");
-const cityName = document.querySelector("[data-name]");
-const weatherCond = document.querySelector("[data-weather]");
-const widget = document.querySelector("[data-widget]");
-
-function drawName(obj) {
-	cityName.textContent = obj.name;
-	localStorage.setItem("cityName", obj.name);
-	return obj;
-}
-
-function drawTemp(obj) {
-	let temperature = document.createElement("li");
-	let temp = obj.main.temp;
-	temp = (((temp - 273.15) * 9) / 5 + 32).toFixed(1);
-	localStorage.setItem("temperature", temp);
-	temperature.textContent = `Temperature: ${temp} °F`;
-	display.appendChild(temperature);
-	return obj;
-}
-
-function drawPress(obj) {
-	let pressure = document.createElement("li");
-	localStorage.setItem("pressure", obj.main.pressure);
-	pressure.textContent = `Pressure: ${obj.main.pressure} hPa.`;
-	display.appendChild(pressure);
-	return obj;
-}
-
-function drawHum(obj) {
-	let humidity = document.createElement("li");
-	localStorage.setItem("humidity", obj.main.humidity);
-	humidity.textContent = `Humidity: ${obj.main.humidity}%`;
-	display.appendChild(humidity);
-	return obj;
-}
-
-function weather(obj) {
-	let weatherObj = obj.weather[0];
-	let img = document.createElement("img");
-	let iconID = weatherObj.icon;
-	img.setAttribute("src", `http://openweathermap.org/img/w/${iconID}.png`);
-	let weatherHeader = document.createElement("h5");
-	weatherHeader.textContent = `${weatherObj.description}`;
-	localStorage.setItem("weather", weatherHeader.textContent);
-	weatherCond.appendChild(img);
-	weatherCond.appendChild(weatherHeader);
-}
-
-function loadCache() {
-	cityName.textContent = localStorage.getItem("cityName");
-
-	let weatherHeader = document.createElement("h5");
-	weatherHeader.textContent = localStorage.getItem("weather");
-
-	let temperature = document.createElement("li");
-	temperature.textContent = `Temperature: ${localStorage.getItem(
-		"temperature"
-	)} °F`;
-	let humidity = document.createElement("li");
-	humidity.textContent = `Humidity: ${localStorage.getItem("humidity")} %`;
-	let pressure = document.createElement("li");
-	pressure.textContent = `Pressure: ${localStorage.getItem("pressure")} hPa`;
-
-	display.appendChild(temperature);
-	display.appendChild(humidity);
-	display.appendChild(pressure);
-}
-
-getWeather();
+// const map = new google.maps.Map(mapContainer, {
+// 	center: { lat: -34.397, lng: 150.644 },
+// 	zoom: 8
+// });
---
+++ scripts/weather.js
@@ -0,0 +1,88 @@
+// logic for weather component
+// not refactored or tailored for receiving input
+
+const userCity = "atlanta";
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${OWKey}`;
+
+// get and display temperature
+function getWeather() {
+	fetch(url)
+		.then(r => r.json())
+		.then(drawName)
+		.then(drawTemp)
+		.then(drawHum)
+		.then(drawPress)
+		.then(weather)
+		.catch(loadCache);
+}
+const body = document.querySelector("body");
+const display = document.querySelector("[data-display]");
+const cityName = document.querySelector("[data-name]");
+const weatherCond = document.querySelector("[data-weather]");
+const widget = document.querySelector("[data-widget]");
+
+function drawName(obj) {
+	cityName.textContent = obj.name;
+	localStorage.setItem("cityName", obj.name);
+	return obj;
+}
+
+function drawTemp(obj) {
+	let temperature = document.createElement("li");
+	let temp = obj.main.temp;
+	temp = (((temp - 273.15) * 9) / 5 + 32).toFixed(1);
+	localStorage.setItem("temperature", temp);
+	temperature.textContent = `Temperature: ${temp} °F`;
+	display.appendChild(temperature);
+	return obj;
+}
+
+function drawPress(obj) {
+	let pressure = document.createElement("li");
+	localStorage.setItem("pressure", obj.main.pressure);
+	pressure.textContent = `Pressure: ${obj.main.pressure} hPa.`;
+	display.appendChild(pressure);
+	return obj;
+}
+
+function drawHum(obj) {
+	let humidity = document.createElement("li");
+	localStorage.setItem("humidity", obj.main.humidity);
+	humidity.textContent = `Humidity: ${obj.main.humidity}%`;
+	display.appendChild(humidity);
+	return obj;
+}
+
+function weather(obj) {
+	let weatherObj = obj.weather[0];
+	let img = document.createElement("img");
+	let iconID = weatherObj.icon;
+	img.setAttribute("src", `http://openweathermap.org/img/w/${iconID}.png`);
+	let weatherHeader = document.createElement("h5");
+	weatherHeader.textContent = `${weatherObj.description}`;
+	localStorage.setItem("weather", weatherHeader.textContent);
+	weatherCond.appendChild(img);
+	weatherCond.appendChild(weatherHeader);
+}
+
+function loadCache() {
+	cityName.textContent = localStorage.getItem("cityName");
+
+	let weatherHeader = document.createElement("h5");
+	weatherHeader.textContent = localStorage.getItem("weather");
+
+	let temperature = document.createElement("li");
+	temperature.textContent = `Temperature: ${localStorage.getItem(
+		"temperature"
+	)} °F`;
+	let humidity = document.createElement("li");
+	humidity.textContent = `Humidity: ${localStorage.getItem("humidity")} %`;
+	let pressure = document.createElement("li");
+	pressure.textContent = `Pressure: ${localStorage.getItem("pressure")} hPa`;
+
+	display.appendChild(temperature);
+	display.appendChild(humidity);
+	display.appendChild(pressure);
+}
+
+getWeather();
